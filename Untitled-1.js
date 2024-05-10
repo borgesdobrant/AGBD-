8d0b4c2d4bcb564fc02ef952048c66120bc33ae0
borgesdobrant <168040036+borgesdobrant@users.noreply.github.com>
2024-05-09 21:23:14 -0300

    arquivo script

---
+++ Untitled-1.js
@@ -0,0 +1,17 @@
+
+//O vento DOM é acionado quando todo html foi completamente carregado 
+document.addEventListener('DOMContentLoaded', function () {
+
+    alert(123);
+
+    //Tipos de dados 
+    //String: Sequencia de caracter 
+    //Number: números inteiros ou pontos flutuantes(10.4)
+    //Boolean: retorna dois valores, true ou false 
+    //object: estrutura de dados que pode armazenar múltiplos valores 
+    //Null: quando está vazio; 
+    // array: objeto especial ordenado 
+    //function: bloco de código reutilizável 
+
+    
+});
